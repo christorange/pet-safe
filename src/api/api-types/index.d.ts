ca1da2c0d7e2a4e1835ea15ff8f99a33898301f5
christorange <christol.yu@outlook.com>
2023-10-16 13:14:00 -0400

    chore: update api types

--- src/api/api-types/index.d.ts
+++ src/api/api-types/index.d.ts
@@ -4,6 +4,23 @@ import * as fastify_types_type_provider from 'fastify/types/type-provider';
 import * as http from 'http';
 import * as fastify from 'fastify';
 
+interface IFeatures {
+    type: string;
+    geometry: {
+        type: string;
+        coordinates: number[];
+    };
+    properties: {
+        id?: number;
+        name: string;
+        address: string;
+    };
+}
+interface IGeojson {
+    type: string;
+    features: IFeatures[];
+}
+
 interface User {
     name: string[] | string;
 }
@@ -80,6 +97,61 @@ declare const appRouter: _trpc_server.CreateRouterInner<_trpc_server.RootConfig<
         }, {
             text: string;
         }>;
+        echo: _trpc_server.BuildProcedure<"query", {
+            _config: _trpc_server.RootConfig<{
+                ctx: {
+                    req: fastify.FastifyRequest<fastify.RouteGenericInterface, fastify.RawServerDefault, http.IncomingMessage, fastify.FastifySchema, fastify.FastifyTypeProviderDefault, unknown, fastify.FastifyBaseLogger, fastify_types_type_provider.ResolveFastifyRequestType<fastify.FastifyTypeProviderDefault, fastify.FastifySchema, fastify.RouteGenericInterface>>;
+                    res: fastify.FastifyReply<fastify.RawServerDefault, http.IncomingMessage, http.ServerResponse<http.IncomingMessage>, fastify.RouteGenericInterface, unknown, fastify.FastifySchema, fastify.FastifyTypeProviderDefault, unknown>;
+                    user: User;
+                };
+                meta: object;
+                errorShape: _trpc_server.DefaultErrorShape;
+                transformer: typeof superjson.default;
+            }>;
+            _ctx_out: {
+                req: fastify.FastifyRequest<fastify.RouteGenericInterface, fastify.RawServerDefault, http.IncomingMessage, fastify.FastifySchema, fastify.FastifyTypeProviderDefault, unknown, fastify.FastifyBaseLogger, fastify_types_type_provider.ResolveFastifyRequestType<fastify.FastifyTypeProviderDefault, fastify.FastifySchema, fastify.RouteGenericInterface>>;
+                res: fastify.FastifyReply<fastify.RawServerDefault, http.IncomingMessage, http.ServerResponse<http.IncomingMessage>, fastify.RouteGenericInterface, unknown, fastify.FastifySchema, fastify.FastifyTypeProviderDefault, unknown>;
+                user: User;
+            };
+            _input_in: typeof _trpc_server.unsetMarker;
+            _input_out: typeof _trpc_server.unsetMarker;
+            _output_in: typeof _trpc_server.unsetMarker;
+            _output_out: typeof _trpc_server.unsetMarker;
+            _meta: object;
+        }, undefined>;
+    }>;
+    places: _trpc_server.CreateRouterInner<_trpc_server.RootConfig<{
+        ctx: {
+            req: fastify.FastifyRequest<fastify.RouteGenericInterface, fastify.RawServerDefault, http.IncomingMessage, fastify.FastifySchema, fastify.FastifyTypeProviderDefault, unknown, fastify.FastifyBaseLogger, fastify_types_type_provider.ResolveFastifyRequestType<fastify.FastifyTypeProviderDefault, fastify.FastifySchema, fastify.RouteGenericInterface>>;
+            res: fastify.FastifyReply<fastify.RawServerDefault, http.IncomingMessage, http.ServerResponse<http.IncomingMessage>, fastify.RouteGenericInterface, unknown, fastify.FastifySchema, fastify.FastifyTypeProviderDefault, unknown>;
+            user: User;
+        };
+        meta: object;
+        errorShape: _trpc_server.DefaultErrorShape;
+        transformer: typeof superjson.default;
+    }>, {
+        allPlaces: _trpc_server.BuildProcedure<"query", {
+            _config: _trpc_server.RootConfig<{
+                ctx: {
+                    req: fastify.FastifyRequest<fastify.RouteGenericInterface, fastify.RawServerDefault, http.IncomingMessage, fastify.FastifySchema, fastify.FastifyTypeProviderDefault, unknown, fastify.FastifyBaseLogger, fastify_types_type_provider.ResolveFastifyRequestType<fastify.FastifyTypeProviderDefault, fastify.FastifySchema, fastify.RouteGenericInterface>>;
+                    res: fastify.FastifyReply<fastify.RawServerDefault, http.IncomingMessage, http.ServerResponse<http.IncomingMessage>, fastify.RouteGenericInterface, unknown, fastify.FastifySchema, fastify.FastifyTypeProviderDefault, unknown>;
+                    user: User;
+                };
+                meta: object;
+                errorShape: _trpc_server.DefaultErrorShape;
+                transformer: typeof superjson.default;
+            }>;
+            _ctx_out: {
+                req: fastify.FastifyRequest<fastify.RouteGenericInterface, fastify.RawServerDefault, http.IncomingMessage, fastify.FastifySchema, fastify.FastifyTypeProviderDefault, unknown, fastify.FastifyBaseLogger, fastify_types_type_provider.ResolveFastifyRequestType<fastify.FastifyTypeProviderDefault, fastify.FastifySchema, fastify.RouteGenericInterface>>;
+                res: fastify.FastifyReply<fastify.RawServerDefault, http.IncomingMessage, http.ServerResponse<http.IncomingMessage>, fastify.RouteGenericInterface, unknown, fastify.FastifySchema, fastify.FastifyTypeProviderDefault, unknown>;
+                user: User;
+            };
+            _input_in: typeof _trpc_server.unsetMarker;
+            _input_out: typeof _trpc_server.unsetMarker;
+            _output_in: typeof _trpc_server.unsetMarker;
+            _output_out: typeof _trpc_server.unsetMarker;
+            _meta: object;
+        }, IGeojson>;
     }>;
 }>;
 type AppRouter = typeof appRouter;
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,7 +1,9 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { trpc } from '../api';
 const Tab3: React.FC = () => {
-  const {data: text} = trpc.api.hello.useQuery()
+  // const {data: text} = trpc.api.hello.useQuery()
+  const {data:text} = trpc.api.version.useQuery();
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,7 +12,7 @@ const Tab3: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <div className='btn mb-20 w-20 ml-5'>button</div>
-      {/* {text? <h3>{JSON.stringify(text)}</h3> : null} */}
+      {text? <h3>{JSON.stringify(text)}</h3> : null}
     </IonPage>
   );
 };
