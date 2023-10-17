cac622e584d27b170fc4e94c6df034b1157dac88
Yukun Shan <nathanielcrush51@gmail.com>
2023-10-17 00:37:15 -0400

    added geo dependencies and added user location in mainMap

--- src/components/MainMap.tsx
+++ src/components/MainMap.tsx
@@ -1,12 +1,18 @@
 import Map, { Source, Layer } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { trpc } from '../api';
 import type { CircleLayer } from 'react-map-gl';
 
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+import React from "react";
+
+
+
+
 const MainMap: FC = () => {
   const {data: allPlacesData, isLoading} = trpc.places.allPlaces.useQuery()
-
+  
   const allPlacesLayer: CircleLayer = {
     id: 'allPlacesLayer',
     type: 'circle',
@@ -16,14 +22,38 @@ const MainMap: FC = () => {
     },
     source: 'allPlaces'
   }
+
+  // const uLocationLayer: CircleLayer = {
+  //   id: 'uLocationLayer',
+  //   type: 'circle',
+  //   paint: {
+  //     'circle-radius': 10,
+  //     'circle-color': 'red'
+  //   }
+  // }
+  
+// Getting User's location
+  const [position, setPosition] = useState<Geoposition>();
+  
+  const getLocation = async () => {
+    try{   
+      const position = await Geolocation.getCurrentPosition();
+      setPosition(position);
+    }catch (e) {
+      console.log(e);
+    }
+  };
+  //error here, its requesting nonstop
+  const location = getLocation();
+  console.log(location);
   return(
     <>
       { isLoading && <span className="loading loading-ring loading-lg"></span>}
       {  allPlacesData &&
         <Map
           initialViewState={{
-            longitude: -71.0589,
-            latitude: 42.3601,
+            longitude: position ? `${position.coords.longitude}` : -71.0589,
+            latitude: position ? `${position.coords.latitude}` : 42.3601,
             zoom: 12
           }}
           style={{
@@ -40,6 +70,14 @@ const MainMap: FC = () => {
           >
             <Layer {...allPlacesLayer} />
           </Source>
+
+          {/* <Source
+            id='uLocationLayer'
+            type='geojson'
+            data={location}
+          >
+            <Layer {...allPlacesLayer} />
+          </Source> */}
         </Map>}
     </>
   )
