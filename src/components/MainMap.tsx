123a86c91dd80ea354fe83cf780660dfea573c62
Yukun Shan <nathanielcrush51@gmail.com>
2023-10-18 18:31:14 -0400

    feat: Rendering User Location

--- src/components/MainMap.tsx
+++ src/components/MainMap.tsx
@@ -1,6 +1,6 @@
 import Map, { Source, Layer } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { FC, useState } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { trpc } from '../api';
 import type { CircleLayer } from 'react-map-gl';
 
@@ -22,45 +22,51 @@ const MainMap: FC = () => {
     },
     source: 'allPlaces'
   }
-
-  // const uLocationLayer: CircleLayer = {
-  //   id: 'uLocationLayer',
-  //   type: 'circle',
-  //   paint: {
-  //     'circle-radius': 10,
-  //     'circle-color': 'red'
-  //   }
-  // }
   
-// Getting User's location
-  const [position, setPosition] = useState<Geoposition>();
   
-  const getLocation = async () => {
-    try{   
-      const position = await Geolocation.getCurrentPosition();
-      setPosition(position);
-    }catch (e) {
-      console.log(e);
-    }
+  const uLocationLayer: CircleLayer = {
+    id: 'uLocationLayer',
+    type: 'circle',
+    paint: {
+      'circle-radius': 10,
+      'circle-color': 'red',
+    },
+    source: 'uLocation', 
   };
-  //error here, its requesting nonstop
-  const location = getLocation();
-  console.log(location);
+
+
+// Getting User's location
+const [position, setPosition] = useState<Geoposition | null>(null);
+
+const getLocation = async () => {
+  try {
+    const currentPosition = await Geolocation.getCurrentPosition();
+    setPosition(currentPosition);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// Call getLocation when the component mounts using useEffect
+useEffect(() => {
+  getLocation();
+}, []);
+
   return(
     <>
       { isLoading && <span className="loading loading-ring loading-lg"></span>}
       {  allPlacesData &&
         <Map
           initialViewState={{
-            longitude: position ? `${position.coords.longitude}` : -71.0589,
-            latitude: position ? `${position.coords.latitude}` : 42.3601,
+            longitude: position ? parseFloat(`${position.coords.longitude}`) : -71.0589,
+            latitude: position ? parseFloat(`${position.coords.latitude}`) : 42.3601,
             zoom: 12
           }}
           style={{
             width: '100vw',
             height: '100vh'
           }}
-          mapStyle="mapbox://styles/christorange/clnt6569c00e701qugxg8ffty"
+          mapStyle="mapbox://styles/christorange/clnt6653300d601qj6z456rlx"
           mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
         >
           <Source
@@ -71,13 +77,19 @@ const MainMap: FC = () => {
             <Layer {...allPlacesLayer} />
           </Source>
 
-          {/* <Source
-            id='uLocationLayer'
-            type='geojson'
-            data={location}
-          >
-            <Layer {...allPlacesLayer} />
-          </Source> */}
+          {position && ( // Render the user's location only if it's available
+            <Source
+              id="uLocation"
+              type="geojson"
+              data={{
+                type: 'Point',
+                coordinates: [position.coords.longitude, position.coords.latitude],
+              }}
+            >
+              <Layer {...uLocationLayer} />
+            </Source>
+          )}
+
         </Map>}
     </>
   )
