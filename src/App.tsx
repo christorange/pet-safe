aff010d25be92d24ace3d156180a0f2e13f81fd2
Yiminada <yxu168@bu.edu>
2023-11-11 14:23:20 -0500

    2

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import Tab3 from './pages/Tab4';
 
 import './styles/global.css';
 
